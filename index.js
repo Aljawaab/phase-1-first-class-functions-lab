b0f4822b3c3b28a661652e4b55f1f1c1acb6f358
Abdul Aljawaab <abdulkhamis4041@gmail.com>
2024-09-30 21:30:49 +0300

    coded_advance1st2CFunc

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+  }
+  
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectingDrivers,
+    selectDifferentDrivers,
+  };
